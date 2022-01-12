7c1a361493a0b0db96504596a3d9207e1af6413e
Javeria2020-ag-6490 <javeriamalik6692@gmail.com>
2022-01-11 17:18:58 -0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace New_folder__2_
+{
+   public class Person
+    {
+        public string FirstName;
+          public string LastName;
+          public void Introduce()
+          {
+        Console.WriteLine("my name is " + FirstName +" " + LastName);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+        Person javeria = new Person();
+        }
+    }
+}
